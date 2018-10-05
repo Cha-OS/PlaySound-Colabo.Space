29ec350f7db78101701823f6ae663b18a7db3c3e
Sinisa Rudan <sinisa.rudan@gmail.com>
2018-10-05 21:30:55 +0200

    router; play component

---
+++ src/frontend/apps/PlaySound/src/app/app-routing.module.ts
@@ -0,0 +1,71 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+
+import {PlayComponent} from './play/play.component';
+
+import {RimaRegisterComponent} from '@colabo-rima/rima_aaa/rima-register/rima-register.component';
+import { RimaLoginComponent } from '@colabo-rima/rima_aaa/rima-login/rima-login.component';
+
+// import {TopiChatTalkForm} from '@colabo-topiChat/talk';
+
+const routes: Routes = [
+  {//default route
+    path: '',
+    redirectTo: '/play',
+    pathMatch: 'full'
+  },
+  {
+    path: 'play',
+    component: PlayComponent
+  },
+  // same route but with the roomId provided
+  {
+    path: 'play/room/:roomId',
+    component: PlayComponent
+  },
+  // same route but with the soundId provided
+  {
+    path: 'play/sound/:soundId',
+    component: PlayComponent
+  },
+  // same route but with the roomId and soundId provided
+  {
+    path: 'play/room/:roomId/sound/:soundId',
+    component: PlayComponent
+  },
+  {
+    path: 'rima-register',
+    component: RimaRegisterComponent
+  },
+  {
+    path: 'rima-login',
+    component: RimaLoginComponent
+  }
+
+/*
+  - routes
+    -    .when('/chat/:rooms/', {
+
+    -     .when('/chat/:rooms/:sounds', {
+
+    -     .when('/chat/:sounds', {
+
+    -  .when('/chat', {
+
+*/
+
+];
+
+@NgModule({
+  exports: [
+    // makes router directives available for use in
+    // other components that will need them
+    RouterModule
+  ],
+  imports: [
+    // initialize RouterModule with routes
+    RouterModule.forRoot(routes)
+  ]
+})
+
+export class AppRoutingModule { }
