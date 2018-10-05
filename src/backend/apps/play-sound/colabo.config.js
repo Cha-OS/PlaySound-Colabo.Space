e9ae17ed4b18bb7b35d3fd8e9549a41adcdbe33c
Sasha Rudan <mprinc@gmail.com>
2018-10-06 00:50:08 +0100

    Added initial seed of new backend

---
+++ src/backend/apps/play-sound/colabo.config.js
@@ -0,0 +1,13 @@
+var puzzles = {
+    name: "b-play-sound",
+    description: "This is a frontend aspect of the PlaySound system",
+    dependencies: {
+        "@colabo-rima/rima-connect": {},
+        "@colabo-topiChat/b-core": {},
+        "@colabo-topiChat/b-talk": {},
+        "@colabo-utils/config": {}
+    },
+    offers: {}
+}
+
+exports.puzzles = puzzles;
